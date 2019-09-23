a716cf97d9f3617ab3299b32a8676b7f61d22818
Christoffer Lundqvist <39331850+chribbelund@users.noreply.github.com>
2019-09-23 10:29:09 +0200

    navbar början

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 function skillbar() {
-    var elem = document.getElementById("skills");
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-      } else {
-        width++;
-        elem.style.width = width + '%';
-      }
+  var elem = document.getElementById("skills");
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
     }
   }
+}
+
+$(document).ready(function(){
+  $("#hamburger").click(function(){
+    if($("#hamburger").text() == "☰"){
+      $("#hamburger").text("")
+    }
+  })
+})
