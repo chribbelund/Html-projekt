62c1dfa46b0710877195f84430d9e934c06dc033
Mushxd <55689370+Mushxd@users.noreply.github.com>
2019-09-24 11:44:23 +0200

    sag

--- main.js
+++ main.js
@@ -29,3 +29,46 @@ setInterval(function() {
     .end()
     .appendTo('#slideshow');
 },  3000);
+
+
+var playing = true;
+var pauseButton = document.getElementById('pause');
+var myIndex = 0;
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
+}
+
+
+function pauseSlideshow(){
+    pauseButton.innerHTML = 'Play';
+    playing = false;
+    clearInterval(slideInterval);
+}
+
+function playSlideshow(){
+    pauseButton.innerHTML = 'Pause';
+    playing = true;
+    slideInterval = setInterval(carousel, 2000);
+}
+
+pauseButton.onclick = function(){
+    if(playing){
+        pauseSlideshow();
+    }
+    else{
+        playSlideshow();
+    }
+};
+
+carousel();
+playSlideshow();
