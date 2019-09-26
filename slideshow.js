4a40c4ac8ee3e92bfcd8f5f69dbeafc6e9f3c4d5
Christoffer Lundqvist <39331850+chribbelund@users.noreply.github.com>
2019-09-26 20:09:31 +0200

    slideshow
    måste fixa med scaling bara

---
+++ slideshow.js
@@ -0,0 +1,45 @@
+
+
+var width = 720;
+var animationSpeed = 1000;
+var pause = 3000;
+var currentSlide = 1;
+
+var $slider = $('#slider');
+var $slideContainer = $('.slides', $slider);
+var $slides = $('.slide', $slider);
+
+var interval;
+var buttonPressed = false;
+function startSlider() {
+    interval = setInterval(function () {
+        $slideContainer.animate({ 'margin-left': '-=' + width }, animationSpeed, function () {
+            if (++currentSlide === $slides.length) {
+                currentSlide = 1;
+                $slideContainer.css('margin-left', 0);
+            }
+        });
+    }, pause);
+}
+function pauseSlider() {
+    clearInterval(interval);
+}
+
+
+$("#pauseSlideshow").click(function () {
+    if (buttonPressed == false) {
+        pauseSlider();
+        buttonPressed = true;
+        document.getElementById("pauseSlideshow").innerHTML = "Starta";
+    }
+    else {
+        startSlider();
+        buttonPressed = false;
+        document.getElementById("namnOk").innerHTML = "Pausa";
+    }
+});
+
+$(document).ready(function () {
+    startSlider();
+});
+
